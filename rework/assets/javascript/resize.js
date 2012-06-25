9947e1084e7bc2cadcbb3a6ad278386a6d456674
snailface <andrew@snailface.com>
2012-06-25 16:04:02 -0230

    fluid resize (iframe) working for specific sites (will make universal later).

--- rework/assets/javascript/resize.js
+++ rework/assets/javascript/resize.js
@@ -1,40 +1,82 @@
-$(function() {
+/*global jQuery */
+/*! 
+* FitVids 1.0
+*
+* Copyright 2011, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
+* Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
+* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
+*
+* Date: Thu Sept 01 18:00:00 2011 -0500
+*/
 
-	// Find all YouTube videos
-	var $allVideos = $("iframe[src^='http://www.youtube.com']"),
+(function( $ ){
 
-	    // The element that is fluid width
-	    $fluidEl = $("body");
+  $.fn.fitVids = function( options ) {
+    var settings = {
+      customSelector: null
+    }
+    
+    var div = document.createElement('div'),
+        ref = document.getElementsByTagName('base')[0] || document.getElementsByTagName('script')[0];
+        
+  	div.className = 'fit-vids-style';
+    div.innerHTML = '&shy;<style>         \
+      .fluid-width-video-wrapper {        \
+         width: 100%;                     \
+         position: relative;              \
+         padding: 0;                      \
+      }                                   \
+                                          \
+      .fluid-width-video-wrapper iframe,  \
+      .fluid-width-video-wrapper object,  \
+      .fluid-width-video-wrapper embed {  \
+         position: absolute;              \
+         top: 0;                          \
+         left: 0;                         \
+         width: 100%;                     \
+         height: 100%;                    \
+      }                                   \
+    </style>';
+                      
+    ref.parentNode.insertBefore(div,ref);
+    
+    if ( options ) { 
+      $.extend( settings, options );
+    }
+    
+    return this.each(function(){
+      var selectors = [
+        "iframe[src^='http://player.vimeo.com']", 
+        "iframe[src^='http://www.youtube.com']", 
+        "iframe[src^='http://www.kickstarter.com']", 
+        "object", 
+        "embed"
+      ];
+      
+      if (settings.customSelector) {
+        selectors.push(settings.customSelector);
+      }
+      
+      var $allVideos = $(this).find(selectors.join(','));
 
-	// Figure out and save aspect ratio for each video
-	$allVideos.each(function() {
+      $allVideos.each(function(){
+        var $this = $(this);
+        if (this.tagName.toLowerCase() == 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; } 
+        var height = this.tagName.toLowerCase() == 'object' ? $this.attr('height') : $this.height(),
+            aspectRatio = height / $this.width();
+		if(!$this.attr('id')){
+			var videoID = 'fitvid' + Math.floor(Math.random()*999999);
+			$this.attr('id', videoID);
+		}
+        $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
+        $this.removeAttr('height').removeAttr('width');
+      });
+    });
+  
+  }
+})( jQuery );
 
-		$(this)
-			.data('aspectRatio', this.height / this.width)
-			
-			// and remove the hard coded width/height
-			.removeAttr('height')
-			.removeAttr('width');
-
-	});
-
-	// When the window is resized
-	// (You'll probably want to debounce this)
-	$(window).resize(function() {
-
-		var newWidth = $fluidEl.width();
-		
-		// Resize all videos according to their own aspect ratio
-		$allVideos.each(function() {
-
-			var $el = $(this);
-			$el
-				.width(newWidth)
-				.height(newWidth * $el.data('aspectRatio'));
-
-		});
-
-	// Kick off one resize to fix all videos on page load
-	}).resize();
+jQuery(document).ready(function(){
+	jQuery(".content-wrapper").fitVids();
 
 });
